6242d85f38220b2cf25feac8b5b98522dae7cbe7
Jordana Luft <jordana.sluft@gmail.com>
2016-01-03 23:44:51 -0200

    created a Gtk.Builder and imported .ui

--- playlist-popover.py
+++ playlist-popover.py
@@ -1,18 +1,17 @@
 from gi.repository import Gtk
 
 class App:
-    
+
     def __init__(self):
-        self.window = Gtk.Window()
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file('playlist-popover.ui')
+        self.window = self.builder.get_object('window')
         self.window.show_all()
-        self.window.connect("delete-event", Gtk.main_quit)
-        
-        
+        self.window.connect('delete-event', Gtk.main_quit)
+
 
     def start(self):
         Gtk.main()
-    
-   
 
 
 def main():
@@ -22,4 +21,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
